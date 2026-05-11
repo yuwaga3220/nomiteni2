bfadd5c429a557c78402b32bcc93fb6ea1818c47
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-11 17:27:18 +0900

    Enhance session creation logic and add user login tests.
    Update SessionsController to authenticate users and handle invalid login attempts with appropriate flash messages.
    
    Correct route for user resources from 'user' to 'users'.
    
    Introduce integration tests for user login functionality to ensure proper handling of invalid credentials.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
   def create
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
