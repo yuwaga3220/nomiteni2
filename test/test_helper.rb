eced8f0f0f44c368a2781eab002f517de85d1bd0
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-13 14:18:38 +0900

    Update asset management and enhance user interface responsiveness.
    Replace application.js link with vendor JavaScript directory in manifest. Add responsive styles for footer and dropdown menu in custom.scss. Implement JavaScript functionality for menu toggling in custom/menu.js. Update header layout to include a responsive navigation toggle and dropdown for user account management. Enhance user login tests to validate session handling and ensure proper flash message display.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,5 +14,9 @@ module ActiveSupport
     include ApplicationHelper
 
     # Add more helper methods to be used by all tests here...
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
