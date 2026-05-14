a3025e826d441977d55790813e5f89955fac82f5
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-14 14:20:13 +0900

    Add remember me checkbox to login form and adjust session handling
    Enhance the user login experience by adding a 'Remember me' checkbox in the login form. Update the SessionsController to handle the remember me functionality, ensuring users are remembered based on their selection. Additionally, apply styling adjustments to the checkbox for improved layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       reset_session
       remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
