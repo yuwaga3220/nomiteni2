ca77d39b5cb863c98a35067251602aae7ce51356
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-15 22:53:27 +0900

    Implement user authentication checks in UsersController
    Add before actions to ensure users are logged in and are editing their own profiles. Introduce helper method to verify current user. Enhance test coverage for user edit actions, including redirects for unauthorized access. Update user fixture to include additional test user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :currect_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -36,11 +38,28 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, 
-                                   :email, 
-                                   :password, 
-                                   :password_confirmation
-                                  )
+  def user_params
+    params.require(:user).permit(:name, 
+                                 :email, 
+                                 :password, 
+                                 :password_confirmation
+                                )
+  end
+
+  # before filter
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "please log in."
+      redirect_to login_url, status: :see_other
     end
+  end
+
+  def currect_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to(root_url, status: :see_other)
+    end
+  end
+
 end
