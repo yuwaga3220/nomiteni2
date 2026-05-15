b224d3f80bf2c8a6a7d52444b021360759656d0e
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-15 17:13:45 +0900

    Add user profile editing functionality and associated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, 
-                                  :password, :password_confirmation)
+      params.require(:user).permit(:name, 
+                                   :email, 
+                                   :password, 
+                                   :password_confirmation
+                                  )
     end
 end
