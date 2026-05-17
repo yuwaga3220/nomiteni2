d742822d9705e72628927e039c67c017d4644ca7
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-17 16:27:18 +0900

    Add Faker gem for test data generation and implement users index view
    Include the Faker gem in the Gemfile for generating sample user data. Create a new index view for displaying all users, along with a corresponding controller action. Update the header to link to the users index and enhance the custom styles for the users list. Add tests to ensure proper access control for the users index.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobarbar",
+             password_confirmation: "foobarbar")
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,4 +18,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "layout links when logged out" do
+    get root_path
+    assert_select "a[href=?]", root_path, minimum: 1
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path   # ログイン直後はプロフィールに飛ぶので、レイアウト確認用に別ページへ
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count: 0
+  end
+
 end
