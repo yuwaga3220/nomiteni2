d87b3e67a34831e15903d4d7184e0af761a46ef5
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-18 15:43:48 +0900

    Add account activation functionality
    Introduce AccountActivationsController and AccountActivationsHelper for managing account activation processes. Update User model to include activation attributes and methods for handling activation tokens. Modify routes to include account activation resources. Update database schema and seed data to support user activation status. Add tests for the new controller to ensure proper functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,
     presence: true,
     length:   { maximum: 50 }
@@ -44,6 +45,17 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_17_110924) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_18_045425) do
   create_table "users", force: :cascade do |t|
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "activation_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
