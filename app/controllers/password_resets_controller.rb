9c629a9bd881589be4f8dc12b9770f393572e651
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-20 00:07:19 +0900

    Implement password reset functionality enhancements
    Add user retrieval and validation before password reset actions in PasswordResetsController. Introduce password reset expiration check in the User model. Update the edit view to include a form for password entry and confirmation. Enhance UserMailer tests to verify password reset email content and recipients.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
 
   def new
   end
@@ -17,5 +19,46 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      @user.update_columns(reset_digest: nil, reset_sent_at: nil)
+      forget(@user)
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # before filter
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset was expired."
+      redirect_to new_password_reset_url
+    end
   end
 end
