0615fd5564825791fea44429ee4fc1cf5c627e3c
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-21 14:59:19 +0900

    Add Micropost model and associated database structure
    Create a new Micropost model with validations for user association and content length. Implement a migration to create the microposts table with necessary fields and indexes. Update the User model to establish a one-to-many relationship with microposts. Add tests to ensure the validity of microposts and their associations with users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_19_085441) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_21_051547) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.boolean "activated", default: false
     t.datetime "activated_at"
@@ -26,4 +35,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_19_085441) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "microposts", "users"
 end
