56e3f7b985ddbf3bbabed85570bf2bae04a9c8c0
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-22 20:58:10 +0900

    Add logged-in user check for micropost actions and update routes
    Implement a private method in ApplicationController to ensure users are logged in before creating or destroying microposts. Update the MicropostsController to use this method as a before_action for the create and destroy actions. Additionally, add routes for micropost creation and destruction. Enhance tests to verify redirection when not logged in and add a new integration test for user profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,14 +61,6 @@ class UsersController < ApplicationController
 
   # before filter
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "please log in."
-      redirect_to login_url, status: :see_other
-    end
-  end
-
   def currect_user
     @user = User.find(params[:id])
     unless current_user?(@user)
