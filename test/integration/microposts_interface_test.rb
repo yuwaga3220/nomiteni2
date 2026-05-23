5fde8e39a5078d969a3aa0923df2a666de8c1221
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-23 21:04:15 +0900

    Add image upload functionality to microposts
    Enhance the MicropostsController to allow image attachments during micropost creation. Update the Micropost model to include an image association using Active Storage. Modify the micropost form to include a file input for images and update the display view to show attached images. Add integration tests to verify image upload functionality.

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -70,3 +70,22 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    img = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: {
+      micropost: {
+        content: cont, image: img
+      }
+    }
+    assert Micropost.last.image.attached?
+  end
+end
