b9fbce0dd2f225c1c875988b7e91be88d22df9c0
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-25 13:09:05 +0900

    Add follow/unfollow forms and integrate into user profile view
    Introduce follow and unfollow forms for user relationships, allowing users to manage their connections. Update the user profile view to display these forms conditionally based on the user's login status. Enhance routing to support relationship creation and destruction.

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
   resources :microposts,  only: [:create, :destroy]
 end
