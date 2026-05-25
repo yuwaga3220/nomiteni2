ab95722ef3c0472a11cab935e1d69365a23d6478
yuwaga3220 <youwuzhangze@gmail.com>
2026-05-25 12:41:08 +0900

    Add user follow/unfollow functionality and update views
    Implement the ability for users to follow and unfollow each other by adding relationships in the User model. Update the UsersController to correct a typo in the before_action method. Enhance the user interface by introducing a new stats partial to display follower and following counts, and integrate it into the post view. Update routes to include following and followers actions for users. Add tests to ensure the follow/unfollow functionality works as expected.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :currect_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
   def index
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
 
   # before filter
 
-  def currect_user
+  def correct_user
     @user = User.find(params[:id])
     unless current_user?(@user)
       redirect_to(root_url, status: :see_other)
