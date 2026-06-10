a44a158197b7ef172acae8c99c9f8f7054cbd4a6
yuwaga3220 <youwuzhangze@gmail.com>
2026-06-10 18:14:00 +0900

    Update application branding in helper and views to reflect Tennis'Ems; modify guest welcome message and user index title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
-
   # ページごとの完全なタイトルを返す
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title(page_title = "")
+    base_title = "Tennis'Ems"
     if page_title.empty?
       base_title
     else
       "#{page_title} | #{base_title}"
     end
   end
-
 end
