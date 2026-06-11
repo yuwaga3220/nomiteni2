abc4d1a3f766196d3abe3c690c83ac34ec44960c
yuwaga3220 <youwuzhangze@gmail.com>
2026-06-11 14:17:26 +0900

    Remove Active Storage integration from Tournament model and drop related database tables

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,14 +1,7 @@
 class Tournament < ApplicationRecord
   belongs_to :user
-  has_one_attached :image do |attachable|
-    attachable.variant :display, resize_to_limit: [500, 500]
-  end
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :title,       presence: true, length: { maximum: 100 }
-  validates :venue,       length: { maximum: 100 }, allow_blank: true
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
-                                      message: "must be a valid image format" },
-                      size:         { less_than: 5.megabytes,
-                                      message:   "should be less than 5MB" }
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :venue, length: { maximum: 100 }, allow_blank: true
 end
