ebf0feac642da366592aa6c8968913a071b4689c
yuwaga3220 <youwuzhangze@gmail.com>
2026-07-11 18:43:46 +0900

    見出しのh2統一

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -11,8 +11,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_template "users/show"
     assert_select "title", full_title(@user.name)
-    assert_select "h1", text: @user.name
-    assert_select "h1>img.gravatar"
+    assert_select "h2", text: @user.name
+    assert_select "h2>img.gravatar"
     assert_match @user.tournaments.count.to_s, response.body
     assert_select "div.pagination"
     @user.tournaments.paginate(page: 1).each do |tournament|
--- test/system/static_pages_test.rb
+++ test/system/static_pages_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class StaticPagesSystemTest < ApplicationSystemTestCase
   test "visiting the home page" do
     visit root_path
-    assert_selector "h1", text: "Hit'Emsへようこそ！"
+    assert_selector "h3", text: "Hit'Emsへようこそ！"
   end
 end
