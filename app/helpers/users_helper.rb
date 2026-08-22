1ab6d6cd850f1135318c481b1d08b1240853dbba
yuwaga3220 <youwuzhangze@gmail.com>
2026-08-22 11:32:42 +0900

    googleアカウント登録の場合のプロフィール画像のリサイズによるサイズ統一

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,14 @@ module UsersHelper
     # Googleアカウントのプロフィール画像があればそちらを優先し、なければGravatarを使う
     def gravatar_for(user, options = { size: 80 })
       size = options[:size]
+      image_style = "width: #{size}px; height: #{size}px; object-fit: cover;"
       if user.avatar_url.present?
-        image_tag(user.avatar_url, alt: user.name, class: "gravatar")
+        image_tag(user.avatar_url, alt: user.name, class: "gravatar", style: image_style)
       else
         gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
         gravatar_url =
           "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+        image_tag(gravatar_url, alt: user.name, class: "gravatar", style: image_style)
       end
     end
 end
